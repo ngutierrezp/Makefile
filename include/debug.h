3d9cfc000c9a46f174117c26aa93dce2575ffb25
Nicolas <lizwert.g@gmail.com>
2019-06-16 14:47:07 -0400

    Feat: Se eliman carpetas innecesarias

--- include/debug.h
+++
@@ -1,6 +0,0 @@
-
-#ifndef debug_h
-#define debug_h
-void print_current(matrix board,int current_ite, int current_lvl, int locals, int best_lvl, int best_locals);
-void printBoard(matrix current);
-#endif
